374082e0f24dcc932d6372286ce5e36c6fa64934
Rafael <rafaelpfreitas@hotmail.com>
2017-10-14 19:14:31 -0300

    Final

--- src/main.c
+++ src/main.c
@@ -6,9 +6,20 @@ int main() {
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
+  for (int i=1; i<n+1; i++) {
     /* Imprime resultado da n-esima iteracao do scheduler */
-  }
 
+	for(int j=0; j<5; j++){
+		if(i % a[j] == 0){
+			printf("%d", j);
+		}
+	}
+	
+	if((i%a[0]!=0) && (i%a[1]!=0) && (i%a[2]!=0) && (i%a[3]!=0) && (i%a[4]!=0)){
+		printf("-");
+	}
+
+	printf("\n");
+}
   return 0;
 }
